d7a7191522f837aaf0206d9eccecc69867477e95
cyprienlardier <cyprien.lardier@etu.unilim.fr>
2026-09-04 11:40:59 +0200

    two

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 
-
-function App() {
+function User(props) {
   return (
-    <>
-      <h1>Bob's Todos</h1>
-      <img src="image.jpg" alt="Hedy Lamarr" />
-      <ul>
-        <li>Invent new traffic lights </li>
-        <li>Rehearse a movie scene </li>
-        <li>Improve the spectrum technology</li>
-      </ul>
-    </>
+    <div className="bg-amber-900 text-white p-4 m-4 rounded">
+      <h2>{props.name}</h2>
+      <p>Age: {props.age}</p>
+    </div>
   );
 }
 
+function App() {
+ return (
+  <>
+  <User name="Hedy Lamarr" age={101} />
+  <User name="Ada Lovelace" age={207} />
+  <User name="Grace Hopper" age={115} />
+  </>
+ );
+}
 export default App;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,17 @@
+
+
+
+function Profile() {
+ return (
+    <>
+      <h1 className="bg-amber-900 text-white p-4">Hedy Lamarr's Todos</h1>
+      <img src="https://i.imgur.com/yXOvdOSs.jpg" alt="Hedy Lamarr" />
+      <ul>
+        <li>Invent new traffic lights</li>
+        <li>Rehearse a movie scene</li>
+        <li>Improve the spectrum technology</li>
+      </ul>
+    </>
+  );
+}
+
