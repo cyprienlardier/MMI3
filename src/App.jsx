87dc01077145cbf2b3956546bde38caf9724824b
cyprienlardier <cyprien.lardier@etu.unilim.fr>
2026-09-04 11:14:43 +0200

    first

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+function App() {
+  return (
+    <>
+      <h1>Bob's Todos</h1>
+      <img src="image.jpg" alt="Hedy Lamarr" />
+      <ul>
+        <li>Invent new traffic lights </li>
+        <li>Rehearse a movie scene </li>
+        <li>Improve the spectrum technology</li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
